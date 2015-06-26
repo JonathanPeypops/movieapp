e37c04792d5db19b0a78cf31a651a5d5812e52df
Jonathan Peypops <jonathanpeypops@msn.com>
2015-06-26 14:22:13 +0200

    ng functions

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,2 +1,2 @@
 angular.module('movieApp.controllers', [])
-.constant('apiurl','http://angularmoviebackend.azurewebsites.net/api')
+.constant('apiUrl','http://angularmoviebackend.azurewebsites.net/api/')
--- app/controllers/searchCtrl.js
+++ app/controllers/searchCtrl.js
@@ -1,6 +1,18 @@
 angular.module('movieApp.controllers')
-.controller('searchCtrl', searchCtlr);
+    .controller('searchCtrl', searchCtlr);
 
-function searchCtlr($scope) {
-$scope.message = "Hello World";
+function searchCtlr($scope, $http, apiUrl, $log) {
+
+    $scope.message = "Hello World";
+    $scope.searchMovie = function (title) {
+        $log.debug(title);
+
+        var url = apiUrl + 'Movies/search?title=' + title;
+
+        $http.get(url).success(function (data) {
+            $scope.results = data;
+        });
+
+        $log.debug('run');
+    }
 };
